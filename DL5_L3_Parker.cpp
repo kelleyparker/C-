71468bf0e6473c69e4584d672a385669d814c723
Kelley Parker <kelley.parker07@gmail.com>
2023-06-12 15:58:09 -0500

    Update DL5_L3_Parker.cpp

--- DL5_L3_Parker.cpp
+++ DL5_L3_Parker.cpp
@@ -6,10 +6,10 @@
 // Instructor:                  Paul Thayer
 //
 // This program generates 3rd grade level math questions and a report at the end.  The user can quit the program by typing in a negative number.
-
 #include <iostream>
 #include <cstdlib>  // required for the rand() and srand() function
 #include <iomanip> // required for setprecision() function
+#include <ctime> // required for time() function
 
 using namespace std;
 
@@ -20,69 +20,38 @@ int main()
     cout << "You can quit the program at any time by typing in a number lower than 0 as a response to a question." << endl;
     cout << "A report is generated at the end of the quiz.\n" << endl;
 
-    int answer;
-    double questionsCorrect = 0, questionsIncorrect = 0;  // Initializing variables of questions correct and incorrect at 0;
+    srand(time(NULL)); // Seed the random number generator only once at the beginning of the program.
 
-    do {
-            srand(time(0)); // Necessary to randomize numbers every time the program runs instead of relying on the same random algorithm.
-            int number1 = rand() % 100 + 1;     // generates a random integer between 0 and 100
-            int number2 = rand() % 100 + 1;     // generates another random integer between 0 and 100
-            cout << "What is " << number1 << " + " << number2 << "?\n";
-            cin >> answer;
-            cout << endl;  // Provides a blank line between each question.
+    double questionsCorrect = 0, questionsIncorrect = 0;
 
-            if (answer == number1 + number2) {      // If the answer is correct, add 1 to the number of correct questions.
-                questionsCorrect++;
-            }
+    while (true) { // Use a while loop and break statements for better readability and control flow.
 
-            else if (answer != number1 + number2) {   // If the answer is incorrect, add 1 to the number of incorrect questions.
-                questionsIncorrect++;
-            }
-        } while (answer >= 0);  // keep asking questions as long as the user submitted number is 0 or greater; a number less than 0 will end the quiz.
+        int number1 = rand() % 100 + 1;     
+        int number2 = rand() % 100 + 1;     
+        cout << "What is " << number1 << " + " << number2 << "?\n";
+        
+        int answer;
+        cin >> answer;
+        cout << endl;
 
-    questionsIncorrect--; // Must reduce questions incorrect by one, otherwise, the question with the negative number submitted counts when it shouldn't.
+        if (answer < 0) { // Check for negative input before processing the question.
+            break;
+        }
+
+        if (answer == number1 + number2) {      
+            questionsCorrect++;
+        }
+
+        else {   
+            questionsIncorrect++;
+        }
+    }
 
     cout << "You have exited the quiz.  Here is your FINAL REPORT. \n\n";
-    double percentageCorrect = questionsCorrect / (questionsCorrect + questionsIncorrect) * 100; // Formula for determining percentage of correct questions
-    cout << endl;  // Provides blank line for readability.
+    double percentageCorrect = questionsCorrect / (questionsCorrect + questionsIncorrect) * 100; 
+    cout << endl;  
     cout << "==============FINAL REPORT=============\n\n";
     cout << "Questions Correct:               " << questionsCorrect << endl;
     cout << "Questions Incorrect:             " << questionsIncorrect << endl;
-    cout << "Percentage of questions correct: " << fixed << setprecision(2) << percentageCorrect << "%\n\n";  // Display percentage to nearest hundredth.
+    cout << "Percentage of questions correct: " << fixed << setprecision(2) << percentageCorrect << "%\n\n";
 }
-
-/* ============================SAMPLE OUTPUT============================
-This program will submit an unlimited number of 3rd grade level math questions with numbers between 0 and 100.
-You can quit the program at any time by typing in a number lower than 0 as a response to a question.
-A report is generated at the end of the quiz.
-
-What is 11 + 11?
-4
-
-What is 18 + 60?
-6
-
-What is 25 + 9?
-8
-
-What is 32 + 58?
-3
-
-What is 46 + 9?
-55
-
-What is 60 + 7?
-67
-
-What is 67 + 56?
--4
-
-You have exited the quiz.  Here is your FINAL REPORT.
-
-
-==============FINAL REPORT=============
-
-Questions Correct:               2
-Questions Incorrect:             4
-Percentage of questions correct: 33.33%
-*/
